62904f370603f6899068fd6ccf8ee45e732655bc
SMaliksparta <46854133+SMaliksparta@users.noreply.github.com>
2019-03-12 11:25:26 +0000

    Add files via upload

---
+++ AnimalRegistration/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnimalRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AnimalAbout()
+        {
+            ViewBag.Message = "about the animal page.";
+
+            return View();
+        }
+
+        public ActionResult AnimalFacts()
+        {
+            ViewBag.Message = "animal fact page.";
+
+            return View();
+        }
+    }
+}
